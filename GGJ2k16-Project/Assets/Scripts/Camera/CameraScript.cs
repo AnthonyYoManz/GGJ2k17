0f6d1f94afe217219043e911e9003cad6e65d1d9
OCHyams <Orlando2.Cazalet-Hyams@live.uwe.ac.uk>
2017-01-20 21:59:23 +0000

    can't remember what i did

---
+++ GGJ2k16-Project/Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraScript : MonoBehaviour
+{
+    public string           m_playerTag = "Player";
+    private List<Player>    m_players;
+    // Use this for initialization
+    private RoomScript      m_curRoom;
+    private Camera          m_camera;
+    private Vector2         m_cameraExtents;
+   // private RoomScript m_prevRoom;need this?
+    void Awake()
+    {
+        m_players = new List<Player>();
+        m_camera = GetComponent<Camera>();
+    }
+	void Start ()
+    {
+        m_cameraExtents.x = m_camera.orthographicSize;
+        m_cameraExtents.y = m_cameraExtents.x * Screen.width / Screen.height;
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag(m_playerTag);
+        foreach (GameObject player in players)
+        {
+            Player playerScript = player.GetComponent<Player>();
+            if (playerScript)
+            {
+                m_players.Add(playerScript);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*Move to average of player positions*/
+        Vector2 position = Vector2.zero;
+        foreach (var player in m_players)
+        {
+            position += (Vector2)player.transform.position;
+        }
+        position /= m_players.Count;
+
+        Vector2 roomCenter = m_curRoom.m_collider.bounds.center;
+       
+        if (position.y + m_cameraExtents.y >= roomCenter.y + m_curRoom.m_collider.bounds.extents.y)
+        {
+            position.y = roomCenter.y + m_curRoom.m_collider.bounds.extents.y;
+        }
+        else if (position.y - m_cameraExtents.y <= roomCenter.y - m_curRoom.m_collider.bounds.extents.y)
+        {
+            position.y = roomCenter.y - m_curRoom.m_collider.bounds.extents.y;
+        }
+
+        if (position.x + m_cameraExtents.x >= roomCenter.x + m_curRoom.m_collider.bounds.extents.x)
+        {
+            position.x = roomCenter.x + m_curRoom.m_collider.bounds.extents.x;
+        }
+        else if (position.x - m_cameraExtents.x <= roomCenter.x - m_curRoom.m_collider.bounds.extents.x)
+        {
+            position.x = roomCenter.x - m_curRoom.m_collider.bounds.extents.x;
+        }
+    }
+
+    public void MoveToRoom(RoomScript _room)
+    {
+
+    }
+}
---
+++ GGJ2k16-Project/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RoomScript : MonoBehaviour
+{
+
+    public Rect m_size { get; private set;}
+
+
+    public BoxCollider2D m_collider { get; private set; }
+
+    //Don't know if we need any update stuff yet so leave it for now
+	// Use this for initialization
+	void Awake ()
+    {
+        m_collider = GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
