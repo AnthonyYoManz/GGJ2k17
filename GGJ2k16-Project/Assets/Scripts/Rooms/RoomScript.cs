e718029df5a28b2e201865458dca2d7c108917ea
AnthonyYoManz <anthony2.arian@live.uwe.ac.uk>
2017-01-21 14:02:56 +0000

    feelsgoodman

--- GGJ2k16-Project/Assets/Scripts/Rooms/RoomScript.cs
+++ GGJ2k16-Project/Assets/Scripts/Rooms/RoomScript.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class RoomScript : MonoBehaviour
 {
-
-    
+    public GameObject m_explosionPrefab;
+    private List<GameObject> m_roomEnemies;
     public BoxCollider2D m_collider { get; private set; }
 
     //Don't know if we need any update stuff yet so leave it for now
@@ -13,10 +13,33 @@ public class RoomScript : MonoBehaviour
 	void Awake ()
     {
         m_collider = GetComponent<BoxCollider2D>();
+        m_roomEnemies = new List<GameObject>();
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(GameObject enemy in enemies)
+        {
+            Vector3 epos = enemy.transform.position;
+            if(m_collider.bounds.Contains(epos))
+            {
+                m_roomEnemies.Add(enemy);
+            }
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+    public void ExploderiseEnemiesPlease()
+    {
+        foreach(GameObject enemy in m_roomEnemies)
+        {
+            if (m_explosionPrefab)
+            {
+                GameObject explosion = Instantiate(m_explosionPrefab);
+                explosion.transform.position = enemy.transform.position;
+            }
+            Destroy(enemy);
+        }
+    }
 }
